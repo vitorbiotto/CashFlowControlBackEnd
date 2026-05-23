0c8c1864ca7a36ab510f1e52f45777c0ffcbbd10
vitorbiotto <vitor.biotto@hotmail.com>
2026-05-23 16:30:55 -0300

    Adicionada validação de unicidade no nome da categoria

--- src/main/java/com/example/CashFlowControlBackEnd/Exceptions/Enums/GenericExceptionKey.java
+++ src/main/java/com/example/CashFlowControlBackEnd/Exceptions/Enums/GenericExceptionKey.java
@@ -3,6 +3,6 @@ package com.example.CashFlowControlBackEnd.Exceptions.Enums;
 public enum GenericExceptionKey {
 
     //Category
-    CATEGORY_ID_IS_REQUIRED,
-    CATEGORY_NAME_IS_REQUIRED;
+    CATEGORY_NAME_IS_REQUIRED,
+    CATEGORY_NAME_ALREADY_EXISTS;
 }
